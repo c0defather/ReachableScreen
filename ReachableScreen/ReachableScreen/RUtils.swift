aa7c95b16fdeab0867e1a420839734789f2817c1
Kuanysh Zhunussov <kuanysh_zhunussov@baylor.edu>
2018-03-16 19:06:03 -0500

    initial commit

--- ReachableScreen/ReachableScreen/RUtils.swift
+++ ReachableScreen/ReachableScreen/RUtils.swift
@@ -5,5 +5,74 @@
 //  Created by Kuanysh on 3/16/18.
 //  Copyright © 2018 Kuanysh. All rights reserved.
 //
+import UIKit
 
-import Foundation
+extension UIApplication {
+    
+    var screenShot: UIImage?  {
+        
+        if let layer = keyWindow?.layer {
+            let scale = UIScreen.main.scale
+            
+            UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+            if let context = UIGraphicsGetCurrentContext() {
+                layer.render(in: context)
+                let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                return screenshot
+            }
+        }
+        return nil
+    }
+    func topMostViewController() -> UIViewController? {
+        return self.keyWindow?.rootViewController?.topMostViewController()
+    }
+}
+extension UIImage {
+    func cropImage(cropRect:CGRect) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(cropRect.size, false, 0);
+        let context = UIGraphicsGetCurrentContext();
+        
+        context?.translateBy(x: 0.0, y: self.size.height);
+        context?.scaleBy(x: 1.0, y: -1.0);
+        context?.draw(self.cgImage!, in: CGRect(x:0, y:0, width:self.size.width, height:self.size.height), byTiling: false);
+        context?.clip(to: [cropRect]);
+        
+        let croppedImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return croppedImage!;
+    }
+}
+extension UIView {
+    func dropShadow() {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: -1, height: 1)
+        self.layer.shadowRadius = 10
+        
+        self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        self.layer.shouldRasterize = true
+        
+        self.layer.rasterizationScale = UIScreen.main.scale
+    }
+}
+extension UIViewController {
+    func topMostViewController() -> UIViewController {
+        if self.presentedViewController == nil {
+            return self
+        }
+        if let navigation = self.presentedViewController as? UINavigationController {
+            return navigation.visibleViewController!.topMostViewController()
+        }
+        if let tab = self.presentedViewController as? UITabBarController {
+            if let selectedTab = tab.selectedViewController {
+                return selectedTab.topMostViewController()
+            }
+            return tab.topMostViewController()
+        }
+        return self.presentedViewController!.topMostViewController()
+    }
+}
+
--- ReachableScreen/ReachableScreen/RWindow.swift
+++ ReachableScreen/ReachableScreen/RWindow.swift
@@ -6,4 +6,139 @@
 //  Copyright © 2018 Kuanysh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class RWindow: UIWindow {
+    var force: Bool = false
+    var circle: UIView!
+    var finger: UIView!
+    var box: UIImageView!
+    var firstTouch: CGPoint!
+    let height = UIScreen.main.bounds.height
+    let width = UIScreen.main.bounds.width
+    var fullscreen: Bool!
+    var view: UIView!
+    public func load(fullscreen full: Bool) {
+        self.fullscreen = full
+        self.view = UIView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        addSubview(view)
+        circle = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 20.0, height: 20.0))
+        circle.center = self.view.center
+        circle.layer.cornerRadius = 10
+        circle.backgroundColor = UIColor.black
+        circle.alpha = 0.4
+        circle.clipsToBounds = true
+        circle.isHidden = true
+        self.view.addSubview(circle)
+        finger = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0))
+        finger.center = self.view.center
+        finger.layer.cornerRadius = 25
+        finger.backgroundColor = UIColor.black
+        finger.alpha = 0.2
+        finger.clipsToBounds = true
+        finger.isHidden = true
+        self.view.addSubview(finger)
+        if (fullscreen) {
+            box = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: width * 0.5, height: height * 0.5))
+        } else {
+            box = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: width, height: height * 0.2))
+        }
+        box.center = self.view.center
+        box.alpha = 1
+        box.clipsToBounds = true
+        box.isHidden = true
+        self.view.addSubview(box)
+    }
+    
+    override public func sendEvent(_ event: UIEvent) {
+        if event.type == .touches {
+            if let count = event.allTouches?.filter({ $0.phase == .began }).count, count > 0 {
+                touchesBegan(event.allTouches!)
+                super.sendEvent(event)
+            }
+            if let count = event.allTouches?.filter({ $0.phase == .moved }).count, count > 0 {
+                touchesMoved(event.allTouches!)
+                if (!force){
+                    super.sendEvent(event)
+                }
+            }
+            if let count = event.allTouches?.filter({ $0.phase == .ended }).count, count > 0 {
+                touchesEnded(event.allTouches!)
+                super.sendEvent(event)
+            }
+            if let count = event.allTouches?.filter({ $0.phase == .cancelled }).count, count > 0 {
+                super.sendEvent(event)
+            }
+        }
+    }
+    
+    
+    
+    func touchesBegan(_ touches: Set<UITouch>) {
+        if let touch = touches.first {
+            firstTouch = touch.location(in: self.view)
+        }
+    }
+    
+    func touchesMoved(_ touches: Set<UITouch>) {
+        if let touch = touches.first {
+            let point = touch.location(in: self.view)
+            finger.isHidden = true
+            finger.center = CGPoint(x: point.x, y: point.y)
+            if (touch.force/touch.maximumPossibleForce > 0.3 && !force){
+                force = true
+                if (fullscreen) {
+                    box.center = CGPoint(x: point.x, y: point.y)
+                    box.image = UIApplication.shared.screenShot
+                } else {
+                    box.center = CGPoint(x: width/2, y: point.y + 25)
+                    let rect = CGRect(x: 0.0, y: 0.0, width: width, height: height * 0.2)
+                    box.image = UIApplication.shared.screenShot?.cropImage(cropRect: rect)
+                }
+                box.dropShadow()
+                box.isHidden = false
+                circle.isHidden = false
+                finger.alpha = 0.7
+                let generator = UIImpactFeedbackGenerator(style: .medium)
+                generator.impactOccurred()
+            } else {
+                finger.alpha = 0.2
+            }
+            if (force) {
+                let p2 = touch.location(in: self.box)
+                if (fullscreen) {
+                    circle.center = CGPoint(x: p2.x/0.5, y: p2.y/0.5)
+                } else {
+                    circle.center = CGPoint(x: p2.x, y: p2.y )
+                }
+            }
+        }
+    }
+    
+    func touchesEnded(_ touches: Set<UITouch>) {
+        if (force) {
+            detectViewSelected(point: circle.center)?.sendActions(for: .touchUpInside)
+            touchHandler(circle.center)
+        }
+        force = false
+        circle.isHidden = true
+        finger.isHidden = true
+        box.isHidden = true
+    }
+    
+    public var touchHandler: (_ point: CGPoint) -> () = {_ in
+        
+    }
+    
+    func detectViewSelected(point: CGPoint) -> UIButton? {
+        for view in UIApplication.shared.topMostViewController()!.view.subviews {
+            if let btn = view as? UIButton {
+                if (btn.frame.maxX>=point.x && btn.frame.minX<=point.x && btn.frame.maxY>=point.y && btn.frame.minY<=point.y){
+                    return btn
+                }
+            }
+        }
+        return nil
+    }
+}
+
