938e5df04eb5ba8855aa7f8f3a4d4f8429ff2a76
Kuanysh Zhunussov <kuanysh_zhunussov@baylor.edu>
2018-03-16 19:05:50 -0500

    initial commit

---
+++ ReachableScreen/ReachableScreen/RWindow.swift
@@ -0,0 +1,9 @@
+//
+//  RWindow.swift
+//  ReachableScreen
+//
+//  Created by Kuanysh on 3/16/18.
+//  Copyright © 2018 Kuanysh. All rights reserved.
+//
+
+import Foundation
---
+++ ReachableScreen/ReachableScreen/ReachableScreen.h
@@ -0,0 +1,19 @@
+//
+//  ReachableScreen.h
+//  ReachableScreen
+//
+//  Created by Kuanysh on 3/16/18.
+//  Copyright © 2018 Kuanysh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReachableScreen.
+FOUNDATION_EXPORT double ReachableScreenVersionNumber;
+
+//! Project version string for ReachableScreen.
+FOUNDATION_EXPORT const unsigned char ReachableScreenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReachableScreen/PublicHeader.h>
+
+
